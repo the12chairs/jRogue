3886973214268c411b02c18268e4cd253b446a8a
the12chairs <mbondarenko92@gmail.com>
2013-10-01 22:32:30 +0700

    forest

--- scripts/basic_forest.rb
+++ scripts/basic_forest.rb
@@ -15,9 +15,13 @@ puts "Making dungeon..."
 
 forest = Java::lowlevel::Dungeon.new 30, 30
 
-tree_proto = Java::lowlevel::Tile.new 'Tree', adder + 'res/tree.png', false, true
+tree_proto = Java::lowlevel::Tile.new 'Tree', adder + 'res/tree.png', false, false
 
 grass_proto = Java::lowlevel::Tile.new 'Grass', adder + 'res/grass.png', false, true
+
+num_trees = 50
+
+puts "Making grass..."
 
 
 (forest.getHeight).times {
@@ -26,6 +30,16 @@ grass_proto = Java::lowlevel::Tile.new 'Grass', adder + 'res/grass.png', false, 
     |j|
     forest.addTile(Java::lowlevel::Tile.new(grass_proto, i, j))
   }
-
 }
+puts "Grass done!"
+
+
+puts "Seeds trees"
+
+num_trees.times {
+  
+  forest.addTile(Java::lowlevel::Tile.new(tree_proto, Random.rand(1 .. forest.getHeight-1), Random.rand(1 .. forest.getWidth-1)))
+}
+
+puts "Trees done!"
 puts "Done!"
